43da6ac854402c0acb7fd96f6ce9774ed7b71b6d
carzh <wolfivyaura@gmail.com>
2024-03-22 15:12:09 -0700

    changed vocab size for llama to match finetuning recipe

--- onnxruntime/python/tools/transformers/models/llama/llama_torch.py
+++ onnxruntime/python/tools/transformers/models/llama/llama_torch.py
@@ -19,16 +19,21 @@ def setup_torch_model(args, location, use_auth_token, torch_dtype=torch.float32,
 
     for i in range(world_size):
         if i == rank % (world_size):
-            l_config = AutoConfig.from_pretrained(location, use_auth_token=use_auth_token, cache_dir=args.cache_dir)
-            l_config.use_cache = True
+            # l_config = AutoConfig.from_pretrained(location, use_auth_token=use_auth_token, cache_dir=args.cache_dir)
+            l_config = AutoConfig.from_pretrained(location, use_auth_token=use_auth_token, use_cache=False)
+            l_config.use_cache = False
             l_config._attn_implementation = "eager"  # "eager" uses LlamaAttention for attention layer
+            l_config.num_hidden_layers = 8
+            l_config.vocab_size = 32003
             llama = AutoModelForCausalLM.from_pretrained(
                 location,
                 use_auth_token=use_auth_token,
                 config=l_config,
                 torch_dtype=torch_dtype,
                 cache_dir=args.cache_dir,
+                ignore_mismatched_sizes=True
             )
+            llama.resize_token_embeddings(32003)
             if world_size > 1:
                 llama.parallel_model()
             if device:
