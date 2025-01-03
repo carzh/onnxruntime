587f6242fecc3b59478841a2bccfc6f93b8efd4e
carzh <wolfivyaura@gmail.com>
2025-01-03 14:34:03 -0800

    maui and uitest updates

--- csharp/test/Microsoft.ML.OnnxRuntime.Tests.UITest/Platforms/Android/AppiumSetup.cs
+++ csharp/test/Microsoft.ML.OnnxRuntime.Tests.UITest/Platforms/Android/AppiumSetup.cs
@@ -17,19 +17,19 @@ public partial class AppiumSetup
     {
         // If you started an Appium server manually, make sure to comment out the next line
         // This line starts a local Appium server for you as part of the test run
-        AppiumServerHelper.StartAppiumLocalServer();
+        //AppiumServerHelper.StartAppiumLocalServer();
 
         var androidOptions = new AppiumOptions
         {
             // If running on local, uncomment the below line to use with the specified emulator
-            DeviceName = "emulator-5554",
+            //DeviceName = "emulator-5554",
             // Specify windows as the driver, typically don't need to change this
             AutomationName = "UIAutomator2",
             // Always Windows for Windows
             PlatformName = "Android",
             //PlatformVersion = "17.0",
             //// The identifier of the deployed application to test
-            App = "C:\\Users\\carolinezhu\\Documents\\cowboy-coding\\onnxruntime\\csharp\\test\\Microsoft.ML.OnnxRuntime.Tests.MAUI\\bin\\Debug\\net8.0-android\\ORT.CSharp.Tests.MAUI-Signed.apk",
+            //App = "C:\\Users\\carolinezhu\\Documents\\cowboy-coding\\onnxruntime\\csharp\\test\\Microsoft.ML.OnnxRuntime.Tests.MAUI\\bin\\Debug\\net8.0-android\\ORT.CSharp.Tests.MAUI-Signed.apk",
         };
 
         // Note there are many more options that you can use to influence the app under test according to your needs
